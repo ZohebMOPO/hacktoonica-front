133e8232d61542bcc37242c4f629794210cd0c69
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-09 21:29:40 +0530

    Router Added

---
+++ src/Router.js
@@ -0,0 +1,9 @@
+import React from "react";
+import About from "./pages/About";
+import Anime from "./pages/Anime";
+import Game from "./pages/Game";
+function Router() {
+  return <div></div>;
+}
+
+export default Router;
---
+++ src/pages/Anime.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Anime() {
+  return <div></div>;
+}
+
+export default Anime;
