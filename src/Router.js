f6660fc77dd9831770904b3ea300f9331290f8bb
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-09 21:38:22 +0530

    Routes added

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
 import About from "./pages/About";
 import Anime from "./pages/Anime";
+import Error from "./pages/Error";
 import Game from "./pages/Game";
 function Router() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/about" exact component={About} />
+            <Route path="/twin" exact component={Anime} />
+            <Route path="/game" exact component={Game} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default Router;
