0b764e5a82e6d55c1f4615329ba4006ca4d089f9
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-10 23:36:33 +0530

    UI fixes[2]

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div>
+      <img
+        src="https://cdn.dribbble.com/users/1026512/screenshots/10130839/waifu_laifu_404_copia_4x.png"
+        alt="error"
+        height="900"
+        width="1500"
+      />
+      <p style={{ textAlign: "center" }}>
+        <Link to="/">Go to Home </Link>
+      </p>
+    </div>
+  );
 }
 
 export default Error;
